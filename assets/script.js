782435e56475549e8d5918404dcfe2ee97ebef27
Manuel Schmid <manysch3@gmail.com>
2022-04-28 16:43:47 +0200

    implemented file decryption

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@ let activeTab = 'encrypt';
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('input_file').addEventListener('change', file_upload, false);
-    document.getElementById('output-btn').addEventListener('click', disableAll, false);
     document.getElementById('input_password').addEventListener('keyup', passwordInputChange, false);
     disableAll()
 });
@@ -19,9 +18,13 @@ function disableAll() {
 }
 
 function file_upload(e) {
+    document.getElementById('output_file').href = ''
+    inputFile = null
+
     disableAll()
     if(e.target.files.length !== 1){
-        alert('Please select a file to encrypt!');
+        if (activeTab === 'encrypt') alert('Please select a file to encrypt!');
+        else if (activeTab === 'decrypt') alert('Please select a file to decrypt!');
     } else {
         let file = e.target.files[0];
 
@@ -46,6 +49,7 @@ function passwordInputChange() {
 
 function crypt() {
     let password = document.getElementById('input_password').value;
+
     if (inputFile == null) {
         if (activeTab === 'encrypt') alert('Please select a file to encrypt!');
         else if (activeTab === 'decrypt') alert('Please select a file to decrypt!');
@@ -54,30 +58,48 @@ function crypt() {
         alert('Please enter a valid password.')
     } else {
         let reader = new FileReader();
-        reader.onload = function (e) {
 
-            let encrypted = CryptoJS.AES.encrypt(e.target.result, password);
-
-            document.getElementById('output_file').setAttribute('href', 'data:application/octet-stream,' + encrypted)
-            document.getElementById('output_file').setAttribute('download', inputFile.name + '.encrypted');
+        if (activeTab === 'encrypt') {
+            reader.onload = function (e) {
+                let encrypted = CryptoJS.AES.encrypt(e.target.result, password);
+                document.getElementById('output_file').setAttribute('href', 'data:application/octet-stream,' + encrypted)
+                document.getElementById('output_file').setAttribute('download', inputFile.name + '.encrypted');
+            };
+            reader.readAsDataURL(inputFile);
+            showDownloadButton()
+        } else if (activeTab === 'decrypt') {
+            reader.onload = function (e) {
+                let decrypted = CryptoJS.AES.decrypt(e.target.result, password).toString(CryptoJS.enc.Latin1);
+                if(!/^data:/.test(decrypted)){
+                    alert("Invalid password or file! Please try again.");
+                    return false;
+                }
+                document.getElementById('output_file').setAttribute('href', decrypted)
+                document.getElementById('output_file').setAttribute('download', inputFile.name.replace('.encrypted',''))
+                showDownloadButton()
+            };
+            reader.readAsText(inputFile);
         }
-        reader.readAsDataURL(inputFile);
-
-        document.getElementById('crypt-btn').className = 'btn btn-green vertical-center to-disable hidden'
-        document.getElementById('output-btn').className = 'btn btn-red vertical-center'
     }
+}
+
+function showDownloadButton() {
+    document.getElementById('crypt-btn').className = 'btn btn-green vertical-center to-disable hidden'
+    document.getElementById('output-btn').className = 'btn btn-red vertical-center'
 }
 
 function switchTab(tab) {
     toggleNavItemDisabled()
     if (tab === 'encrypt') {
         document.getElementById('input-file-label').innerText = 'File to encrypt'
+        document.getElementById('input_file').accept = '*'
         document.getElementById('input_password').style.width = '70%';
         document.getElementById('generate-password').style.width = '30%';
+        document.getElementById('generate-password').style.display = 'inherit';
         document.getElementById('crypt-btn').innerHTML = 'Encrypt'
-        // document.getElementById('generate-password').style.display = 'none';
     } else if (tab === 'decrypt') {
         document.getElementById('input-file-label').innerText = 'File to decrypt'
+        document.getElementById('input_file').accept = '.encrypted'
         document.getElementById('input_password').style.width = '100%';
         document.getElementById('generate-password').style.display = 'none';
         document.getElementById('crypt-btn').innerHTML = 'Decrypt'
@@ -90,6 +112,11 @@ function toggleNavItemDisabled() {
         el.disabled = !el.disabled;
         el.classList.toggle('active-tab')
     }
+}
+
+function downloadFile() {
+    document.getElementById('output_file').click()
+    disableAll()
 }
 
 // function decrypt() {
